179eb7221580af3def68761da80b0a3bd5c6502c
Zainab Salih <Zainab.Salih@outlook.com>
2025-04-20 10:08:04 +0400

    quicksort.c updated

--- quicksort.c
+++ quicksort.c
@@ -38,32 +38,31 @@ int	is_sorted_b(t_stack *stack, int size)
 	return (1);
 }
 
-void	quicksort_b(t_stack *stack_b, t_stack *stack_a, int size)
+int	quicksort_b(t_stack *stack_b, t_stack *stack_a, int size)
 {
 	int	pushed;
+	int	remaining;
 
 	if (!stack_b || !stack_b->top || size <= 1)
-		return ;
+		return (size);
 	if (is_sorted_b(stack_b, size))
-		return ;
+		return (size);
 	if (size <= 3)
 	{
 		insertion_sort_desc(stack_b, stack_a, stack_b, size);
-		return ;
+		return (size);
 	}
 	pushed = partition_b(stack_b, stack_a, stack_b, size);
 	quicksort_a(stack_a, stack_b, pushed);
-	quicksort_b(stack_b, stack_a, (size - pushed));
-	if (is_sorted_a(stack_a, pushed) && stack_b->size <= size)
-		return ;
-	while (pushed--)
-		push(stack_a, stack_b, stack_a, stack_b);
-	return ;
+	remaining = 0;
+	remaining += quicksort_b(stack_b, stack_a, (size - pushed));
+	return (remaining);
 }
 
 void	quicksort_a(t_stack *stack_a, t_stack *stack_b, int size)
 {
 	int	pushed;
+	int remaining;
 
 	if (!stack_a || !stack_a->top || size <= 1)
 		return ;
@@ -76,8 +75,8 @@ void	quicksort_a(t_stack *stack_a, t_stack *stack_b, int size)
 	}
 	pushed = partition_a(stack_a, stack_a, stack_b, size);
 	quicksort_a(stack_a, stack_b, (size - pushed));
-	quicksort_b(stack_b, stack_a, pushed);
-	while (pushed--)
+	remaining = quicksort_b(stack_b, stack_a, pushed);
+	while (remaining--)
 		push(stack_b, stack_a, stack_a, stack_b);
 	return ;
 }
